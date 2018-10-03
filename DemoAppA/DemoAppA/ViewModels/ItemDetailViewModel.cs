cf307934080bb767c6680136b13a0fbea6b7c205
Dave Gunther <david.gunther@softwaremotor.com>
2018-10-03 15:43:22 -0400

    initial commit of 3 projects

---
+++ DemoAppA/DemoAppA/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using DemoAppA.Models;
+
+namespace DemoAppA.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
---
+++ DemoAppC/DemoAppC/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using DemoAppC.Models;
+
+namespace DemoAppC.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
